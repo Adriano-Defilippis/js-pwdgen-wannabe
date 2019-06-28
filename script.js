21c264a7ed50ac56a8fc4095f8249631f6ebe8c9
Adriano-Defilippis <ciprianoadrianodefilippis@gmail.com>
2019-06-28 20:06:50 +0200

    Controllo Campi Vuoti (if, else if)

--- script.js
+++ script.js
@@ -12,6 +12,43 @@
 // elPasw.innerHTML = 'elNome + elCognome + elColore + 19';
 
 
+
+
+// function myFunction(){
+//   var elNome, elCognome, elColore, elPasw;
+//
+//   elNome = document.dati.nome.value;
+//
+//   elCognome = document.getElementById('cognome').value;
+//
+//   elColore = document.getElementById('colore').value;
+//
+//   elPasw = elNome + elCognome + elColore + '19';
+//
+//   if (elNome='') {
+//     alert('Non hai inserito il tuo nome..!')
+//   }
+//
+//   if (elCognome='') {
+//     alert('Non hai inserito il tuo cognome.!')
+//   }
+//
+//   if (elColore='') {
+//     alert('Non hai inserito il tuo colore..!')
+//   }else {
+//     document.getElementById('password').innerHTML= elPasw;
+//   }
+//
+//
+// }
+
+
+
+
+
+
+
+
 function myFunction(){
   var elNome, elCognome, elColore, elPasw;
 
@@ -23,6 +60,26 @@ function myFunction(){
 
   elPasw = elNome + elCognome + elColore + '19';
 
-  document.getElementById('password').innerHTML= elPasw;
+  if (elNome=="") {
+    alert('Non hai inserito completato i campi..!');
+    document.form.nome.focus();
+    return false;
+  }
+
+  else if (elCognome=="") {
+    alert('Non hai inserito il tuo cognome.!');
+    document.form.cognome.focus();
+    return false;
+  }
+
+  else if (elColore=="") {
+    alert('Non hai inserito il tuo colore..!');
+    document.form.colore.focus();
+    return false;
+  }else {
+    document.getElementById('password').innerHTML= elPasw;
+  }
+
+
 
 }
